8c478882a1967e6a02fe9f9d42cd20111cc88899
ViniciusCarvalhoMK <vinicius.carvalho@mksolutions.com.br>
2024-11-14 22:46:15 -0300

    Add PHPUnit testing framework and initial unit tests

---
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\Domain\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class CategoryUnitTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInitacd6b74d10fd999bfb77e3a9ef580e6f
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitacd6b74d10fd999bfb77e3a9ef580e6f::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
